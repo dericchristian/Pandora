e6dbbd4641ae148df830a63b1d1e53e5639aa658
Deric Christian <dericchristian@outlook.com>
2019-10-04 19:35:17 +1000

    Add project files.

---
+++ Pandora/contactformhandler.php
@@ -0,0 +1,25 @@
+﻿<?php $name = $_POST['Name'];
+$email = $_POST['Contact-Email'];
+$message = $_POST['Message-body'];
+
+
+$email_from = 'info@pandporafinancialservices.com';
+
+$email_subject = 'Contact form submission acknowledgement';
+
+$email_body = "Name: $name.\n".
+				"Email ID: $email.\n".
+					"Message: $message.\n";
+
+$to = "dericchristian@gmail.com";
+
+$headers = "$From: $email_from \r\n";
+
+$headers .= "$Reply-To: $email \r\n";
+
+mail($to, $email_subject, $email_body, $headers);
+
+header("Location: contact.html");
+
+
+?>
